e9baf3f49665966e2db1a8b9084679dcf7d8feff
Jerry Santiago <jerrysantiago128@gmail.com>
2025-07-02 08:48:48 -0400

    script 4: adding notes on potential ssl issue resolution for cloning

--- 4-build-image.sh
+++ 4-build-image.sh
@@ -3,5 +3,16 @@
 cd compose/
 docker build -t rovaly-web-app:0.0.1 .
 cd ..
+
+# if this fails, you may need to run the following
+#docker build --ssh default=$SSH_AUTH_SOCK -t rovaly-web-app:0.0.1
+# this is becuase the id_rsa and id_rsa.pub is differnt from the ssh-agent pid running
+# evaluate the following:
+
+# eval "$(eval ssh-agent -s)"
+# ssh-add ~/.ssh/id_rsa
+# ssh-add -l
+
+# this will update and add the new ssh agent pid to your id_rsa file
 echo "NEW IMAGE BUILT SUCCESSFULLY"
 echo "run 5-deploy-services.sh to start service suite"

bd49eee1aff2cd621c425b42465d492ded545eab
Jerry Santiago <jerrysantiago128@gmail.com>
2025-07-02 23:46:47 +0000

    move: moving copy of original docker content to the 'test' folder for version control and separation; the test instance should run on 8081

---
+++ compose/test/DataAccess.php
@@ -0,0 +1,36 @@
+<?php
+//https://www.doctrine-project.org/projects/doctrine-dbal/en/4.2/index.html
+require_once(__DIR__ . '/../Packages/vendor/autoload.php');
+require_once(__DIR__ . '/../Models/QueryType.php');
+use Doctrine\DBAL\DriverManager;
+
+class DataAccess {
+    public function GetConnection() {
+            $connectionParams = [
+        'dbname' => 'rovalydb',
+        'user' => 'root',
+        'password' => '',
+        'host' => 'rovaly-db:3306', // change this to 3306
+        'driver' => 'pdo_mysql'
+    ];
+    return DriverManager::GetConnection($connectionParams);
+
+    }
+    
+
+    public function ExecuteQuery(string $query, QueryType $queryType) {
+        $conn = $this->GetConnection();
+        $stmt = $conn->ExecuteQuery($query);
+        
+        // Return the key of the record that was inserted if doing an INSERT query
+        if($queryType == QueryType::INSERT) {
+            $lastInsertedId = $conn->lastInsertId();
+            $conn->close();
+            return $lastInsertedId;
+        }
+        $conn->close();
+        return $stmt;
+    }
+
+}
+?>
